c9a0616c5844577934150ade9f9fbf108498a192
Arslan Younas <arsalyou03@gmail.com>
2022-10-10 13:58:46 +0500

    Completed all tasks

--- task1Q4.js
+++ task1Q4.js
@@ -1,2 +1,6 @@
 //How to solve the high volume of write operations in RDS MySQL databases?
 
+//One way to solve high volume write operations is toincrease the size of the writer instance.
+// Spreading data across multiple physical disks will improve .
+
+
